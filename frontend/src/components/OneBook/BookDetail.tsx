055fe24d79ea3553df481d8ea983cf43129c1323
joratapok <joratapok@gmail.com>
2021-04-19 23:24:47 +0700

    modal reader fix

--- frontend/src/components/OneBook/BookDetail.tsx
+++ frontend/src/components/OneBook/BookDetail.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
 import classes from './BookDetail.module.css'
 import {OneBookType,} from "../../redux/bookReducer"
-import {commentsInitialType} from "../../redux/commentReducer";
-import {addCommentDataType} from "./Comments/CommentForm";
-import Comments from "./Comments/Comments";
+import {commentsInitialType} from "../../redux/commentReducer"
+import {addCommentDataType} from "./Comments/CommentForm"
+import Comments from "./Comments/Comments"
+import Modal from "./Modal/Modal"
 
 type BookDetailType = {
     book: OneBookType
@@ -14,9 +15,7 @@ type BookDetailType = {
 export const BookDetail: React.FC<BookDetailType> = ({book, comments, addComment}) => {
 
     const [modal, setModal] = useState(false)
-
-    let modalClass = classes.modal + ' ' + ( modal ? classes.activeModal : '')
-    let contentModalClass = classes.ContentModal + ' ' + ( modal ? classes.activeContentModal : '')
+    let buttonModalOffClass = classes.buttonModalOff + ' ' + ( modal ? classes.activeButtonModalOff : '')
 
     return (
         <div className={classes.bookWrapper}>
@@ -35,23 +34,12 @@ export const BookDetail: React.FC<BookDetailType> = ({book, comments, addComment
                     </div>
                 </div>
             </div>
-
-            <div className={modalClass}>
-
-                <div className={contentModalClass}>
-                <button onClick={() => setModal(false)}>Не читать</button>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat lacus vel lacus ultricies aliquam. In lectus diam, commodo non augue non, lobortis porta metus. Vivamus augue nisi, rutrum id dolor vel, tristique efficitur nunc. Vivamus lacinia suscipit urna sit amet malesuada. Vivamus vitae eros iaculis, pretium nunc eget, mollis quam. Curabitur aliquet, lacus et ultricies lacinia, felis urna gravida magna, sit amet vulputate tortor elit sit amet arcu. Aliquam sit amet tellus id lacus imperdiet accumsan. Curabitur rutrum elementum urna vel imperdiet. Pellentesque consectetur luctus ex eu feugiat. Aenean consectetur quam vel enim eleifend dictum. Fusce eget diam ut nunc facilisis ultrices sed in dolor. Phasellus eu odio rhoncus, accumsan risus suscipit, placerat nisi. Cras efficitur, arcu vel rhoncus gravida, urna risus pellentesque purus, vitae ultricies elit libero aliquam metus.
-                Nunc vel urna eget sem sagittis fermentum. Ut sit amet porta dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas auctor lobortis molestie. Nulla viverra ultrices luctus. In hendrerit vestibulum risus. Cras non consequat nibh.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat lacus vel lacus ultricies aliquam. In lectus diam, commodo non augue non, lobortis porta metus. Vivamus augue nisi, rutrum id dolor vel, tristique efficitur nunc. Vivamus lacinia suscipit urna sit amet malesuada. Vivamus vitae eros iaculis, pretium nunc eget, mollis quam. Curabitur aliquet, lacus et ultricies lacinia, felis urna gravida magna, sit amet vulputate tortor elit sit amet arcu. Aliquam sit
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat lacus vel lacus ultricies aliquam. In lectus diam, commodo non augue non, lobortis porta metus. Vivamus augue nisi, rutrum id dolor vel, tristique efficitur nunc. Vivamus lacinia suscipit urna sit amet malesuada. Vivamus vitae eros iaculis, pretium nunc eget, mollis quam. Curabitur aliquet, lacus et ultricies lacinia, felis urna gravida magna, sit amet vulputate tortor elit sit amet arcu. Aliquam sit
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat lacus vel lacus ultricies aliquam. In lectus diam, commodo non augue non, lobortis porta metus. Vivamus augue nisi, rutrum id dolor vel, tristique efficitur nunc. Vivamus lacinia suscipit urna sit amet malesuada. Vivamus vitae eros iaculis, pretium nunc eget, mollis quam. Curabitur aliquet, lacus et ultricies lacinia, felis urna gravida magna, sit amet vulputate tortor elit sit amet arcu. Aliquam sit
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat lacus vel lacus ultricies aliquam. In lectus diam, commodo non augue non, lobortis porta metus. Vivamus augue nisi, rutrum id dolor vel, tristique efficitur nunc. Vivamus lacinia suscipit urna sit amet malesuada. Vivamus vitae eros iaculis, pretium nunc eget, mollis quam. Curabitur aliquet, lacus et ultricies lacinia, felis urna gravida magna, sit amet vulputate tortor elit sit amet arcu. Aliquam sit
-                </div>
-            </div>
-
             <div>
                 <Comments addComment={addComment} comments={comments}/>
             </div>
+            <Modal setOn={modal}/>
+            <button className={buttonModalOffClass}
+                    onClick={() => setModal(false)}>Не Читать</button>
         </div>
     )
 }
--- frontend/src/components/OneBook/Comments/CommentForm.tsx
+++ frontend/src/components/OneBook/Comments/CommentForm.tsx
@@ -12,17 +12,19 @@ export type addCommentDataType = {
     text: string
 }
 
-const CommentForm: React.FC<PropsType> = ({comments, addComment}) => {
+const CommentForm: React.FC<PropsType> = ({addComment}) => {
 
-    return(
+    return (
         <Form
             onSubmit={addComment}
             render={({handleSubmit, submitError, form, submitting, pristine, values}) => (
                 <form onSubmit={async (event) => {
                     const error = await handleSubmit(event);
-                    if (error) { return error; }
+                    if (error) {
+                        return error;
+                    }
                     form.reset();
-                  }}>
+                }}>
                     <div>
                         <label>Комментарий</label>
                         <Field name={'text'}
