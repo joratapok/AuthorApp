c5a7b4f1871358df86d310c61e3acc1ea6c8dce8
joratapok <joratapok@gmail.com>
2021-06-07 23:44:24 +0700

    try to add route animation with react-spring

--- react-books/src/components/Body.tsx
+++ react-books/src/components/Body.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 import classes from './Body.module.css'
 import BgBody from './bgBody/BgBody'
 import BooksContainer from './Books/BooksContainer'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useLocation } from 'react-router-dom'
 import NavMenuContainer from './NavMenu/NavMenuContainer'
 import OneBookContainer from './OneBook/OneBookContainer'
 import PageNotFound from './PageNotFound/PageNotFound'
 import Footer from './Footer/Footer'
 import { CssBaseline } from '@material-ui/core'
 import SiteName from './SiteName/SiteName'
+import { useTransition, animated } from 'react-spring'
 
-const Body: React.FC = () => {
+const Body = () => {
+    const location: any = useLocation()
+    // @ts-ignore
+    const transitions: Array<any> = useTransition(location, (location) => location.pathname, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 }
+    })
     return (
         <div className={classes.wrapper}>
             <CssBaseline/>
@@ -18,11 +26,15 @@ const Body: React.FC = () => {
             <div className={classes.content}>
                 <NavMenuContainer/>
                 <SiteName/>
-                <Switch>
-                    <Route path='/book/:bookId?' render={() => <OneBookContainer/>}/>
-                    <Route exact path='/' render={() => <BooksContainer/>}/>
-                    <Route path="*" component={PageNotFound} />
-                </Switch>
+                {transitions.map(({ item, props, key }) => (
+                    <animated.div key={key} style={props}>
+                        <Switch location={item}>
+                            <Route path='/book/:bookId?' render={() => <OneBookContainer/>}/>
+                            <Route exact path='/' render={() => <BooksContainer/>}/>
+                            <Route path="*" component={PageNotFound}/>
+                        </Switch>
+                    </animated.div>
+                ))}
             </div>
             <Footer/>
         </div>
